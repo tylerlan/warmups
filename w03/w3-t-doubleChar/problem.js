c7992990ce581e7c627fd42e34fbd6a1ac6241d2
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-02 09:27:30 -0700

    Added doubleChar in one line with ternary operator and map after first using forEach and map in multiple lines

--- w03/w3-t-doubleChar/problem.js
+++ w03/w3-t-doubleChar/problem.js
@@ -1,6 +1,6 @@
 // Given a string, you have to return a string in which
 // each character (case-sensitive) is repeated once.
-// if anything but a string is passed to the function, 
+// if anything but a string is passed to the function,
 // return "Ruh roh! That isn't a string!"
 
 // doubleChar("String") ==> "SSttrriinngg"
@@ -13,6 +13,26 @@
 // DONE?? Do it with a RegEx! (a 2 line solution is possible)
 
 function doubleChar(str) {
+
+return typeof str === 'string' ? str.split('').map( (char) => char + char ).join('') : "Ruh roh! That isn't a string!";
+
+// if (typeof str === 'string') {
+//   // var result = [];
+//   //
+//   // let arr = str.split('');
+//   //
+//   // let strStr = arr.forEach( (char) => {
+//   //   result.push(char, char);
+//   // } );
+//   //
+//   // return result.join('');
+//
+//   let result = str.split('').map( (char) => char + char ).join('');
+//   return result;
+//
+// } else {
+//   return "Ruh roh! That isn't a string!";
+// }
 
 }
 
