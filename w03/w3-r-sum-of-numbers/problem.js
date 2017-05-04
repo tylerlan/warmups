e7bb3f823f4e63943e06ca110b4cc8fbd91f2709
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-04 09:28:54 -0700

    Added getSum function, and changed the test to require the problem rather than the solution--which fails the test

--- w03/w3-r-sum-of-numbers/problem.js
+++ w03/w3-r-sum-of-numbers/problem.js
@@ -1,10 +1,10 @@
-/* Given two integers, which can be positive and negative, 
+/* Given two integers, which can be positive and negative,
 find the sum of all the numbers between and including a and b,
 and return the sum. If both numbers are equal return a or b.
 
 Note! a and b are not ordered!
 
-Example: 
+Example:
 getSum(1, 0) == 1   // 1 + 0 = 1
 getSum(1, 2) == 3   // 1 + 2 = 3
 getSum(0, 1) == 1   // 0 + 1 = 1
@@ -14,6 +14,13 @@ getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 */
 
 function getSum( a, b ) {
+  let max = Math.max(a, b);
+  let min = Math.min(a, b)
+  let result = 0;
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 
 } // END FUNCTION
 
@@ -22,5 +29,3 @@ module.exports = {
   getSum:getSum,
   attendance:"WORD UP"
 }
-
-
